df931c2043880c5caadae03fdbd83beb6abd9dca
Andrey Vasilev <philobeat@gmail.com>
2023-03-30 08:54:22 +0300

    Add review and comment serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from reviews.models import User
+from reviews.models import Comment, Review, User
 
 
 class UserSerializer(ModelSerializer):
@@ -8,3 +8,19 @@ class UserSerializer(ModelSerializer):
             'username', 'email', 'first_name', 'last_name', 'bio', 'role',
         )
         model = User
+
+
+class ReviewSerializer(ModelSerializer):
+    class Meta:
+        fields = (
+            'title', 'text', 'author', 'score', 'pub_date',
+        )
+        model = Review
+
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        fields = (
+            'review', 'text', 'author', 'pub_date',
+        )
+        model = Comment
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
-from .models import (User, Review, Comment, Title,
-                     Category, Genre)
+from .models import Category, Comment, Genre, Review, Title, User
 
 
 class Admin(UserAdmin):
