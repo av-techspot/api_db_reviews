b7892b66c2d683cf94f1a7073e953e9878c8ec84
Kozin Artem <kaf-artem@yandex.ru>
2023-03-30 11:21:24 +0300

    Created draft registration users

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,10 +1,34 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 from reviews.models import User
 
+from .exceptions import UserExist
 
-class UserSerializer(ModelSerializer):
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         fields = (
             'username', 'email', 'first_name', 'last_name', 'bio', 'role',
         )
         model = User
+
+
+class RegistrationDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('username', 'email',)
+        model = User
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError(f'{value} isn\'t valid username')
+        if (User.objects.filter(username=value)):
+            raise UserExist('Такой пользователь уже зарегистрирован.')
+        return value
+
+    def validate(self, data):
+        if (
+            data.get('username')
+            and User.objects.filter(username=data.get('username'))
+        ):
+            raise UserExist('Такой пользователь уже зарегистрирован.')
+        return data
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,16 +1,60 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.shortcuts import get_object_or_404
+from rest_framework import exceptions, status
+from rest_framework.decorators import api_view
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+from rest_framework.serializers import get_error_detail
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.tokens import AccessToken
 from reviews.models import User
 
-from .serializers import UserSerializer
+from .exceptions import UserExist
+from .serializers import RegistrationDataSerializer, UserSerializer
 
 
+@api_view(['POST'])
 def registration(request):
-    ...
+    serializer = RegistrationDataSerializer(data=request.data)
+    try:
+        serializer.is_valid(raise_exception=True)
+    except UserExist:
+        return Response({}, status=status.HTTP_200_OK)
+    except exceptions.ValidationError:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if serializer.is_valid():
+        serializer.save()
+        user = get_object_or_404(
+            User,
+            username=serializer.validated_data.get('username'),
+        )
+        #user.email_user(
+        #    subject="Подтвердите регистрацию",
+        #    message="confirmation_code: "
+        #            f"{default_token_generator.make_token(user)}",
+        #    from_email='a@a.ru',
+        #)
+        return Response({}, status=status.HTTP_200_OK)
+    return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['POST'])
 def get_token(request):
-    ...
+    if (
+        not request.data
+        or not request.data.get('username')
+    ):
+        return Response(request.data, status=status.HTTP_400_BAD_REQUEST)
+    user = get_object_or_404(User, username=request.data.get('username'))
+    if default_token_generator.check_token(
+        user,
+        request.data.get('confirmation_code')
+    ):
+        return Response(
+            {'token': f'{AccessToken.for_user(user)}'},
+            status=status.HTTP_200_OK
+        )
+    return Response(request.data, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserViewSet(ModelViewSet):
