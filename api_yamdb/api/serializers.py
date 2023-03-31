900e9007094a4340da3ddd23fc4356d47b7fdf5a
verafadeeva <lunatik.yar@gmail.com>
2023-03-31 11:22:01 +0300

    Add Title serialisers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer
-from reviews.models import Category, Comment, Genre, Review, User
+from reviews.models import (Category, Comment, Genre, Review, Title,
+                            TitleGenre, User)
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -28,7 +29,7 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
 
 
-class ReviewSerializer(ModelSerializer):
+class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         fields = (
             'title', 'text', 'author', 'score', 'pub_date',
@@ -36,7 +37,7 @@ class ReviewSerializer(ModelSerializer):
         model = Review
 
 
-class CommentSerializer(ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = (
             'review', 'text', 'author', 'pub_date',
@@ -48,3 +49,33 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('name', 'slug')
         model = Genre
+
+
+class TitleSerializer(serializers.Serializer):
+    name = serializers.CharField(
+        label='Название', required=True, max_length=256
+    )
+    year = serializers.IntegerField(label='Год', required=True)
+    rating = serializers.IntegerField(source='average_rating', read_only=True)
+    description = serializers.CharField(label='Описание', required=False)
+    category = CategorySerializer(required=True)
+    genre = GenreSerializer(many=True, required=True)
+
+    def validate(self, data):
+        pass
+
+    def create(self, validated_data):
+        slug_category = validated_data.pop('category')
+        slug_genre = validated_data.pop('genre')
+        try:
+            category = Category.objects.get(slug=slug_category)
+            genre = Genre.objects.get(slug=slug_genre)
+        except ObjectDoesNotExist:
+            raise serializers.ValidationError(
+                f'Either the "{slug_category}" or the "{slug_genre}" doesn\'t'
+                ' exist.'
+            )
+
+        title = Title.objects.create(category=category, **validated_data)
+        TitleGenre.objects.create(title=title, genre=genre)
+        return title
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import (get_token, registration, UserViewSet, CategoryViewSet,
-                    GenreViewSet)
+from .views import (get_token, registration, CategoryViewSet, GenreViewSet,
+                    TitleViewSet, UserViewSet)
 
 router = SimpleRouter()
 router.register('users', UserViewSet)
 router.register('categories', CategoryViewSet)
 router.register('genres', GenreViewSet)
+router.register('titles', TitleViewSet)
 
 
 urlpatterns = [
