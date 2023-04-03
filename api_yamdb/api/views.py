722ab3448fd2d99b09a29f491cb24c52821e2384
Andrey Vasilev <philobeat@gmail.com>
2023-04-03 12:26:33 +0300

    Add comments

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -159,3 +159,8 @@ class CommentViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
         return review.comments.all()
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, pk=review_id)
+        serializer.save(author=self.request.user, review=review)
